50efb93a22e868b3b8805d75e7518f1b9e21e6cb
wthd-hhp <1178385238@qq.com>
2025-10-14 10:51:33 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -262,8 +262,9 @@ if submit_button:
             except Exception as e:
                 st.error(f"Model loading failed: {str(e)}")
 
-     except Exception as e:
-         st.error(f"An error occurred: {str(e)}")
+      except Exception as e:
+          st.error(f"An error occurred: {str(e)}")
+
 
 
 
